c5452f232c8d93de26e3e485deed914e7adb1ef4
Quanchen0520 <121756632+Quanchen0520@users.noreply.github.com>
2025-04-19 19:38:04 +0800

    55th_module1_subdistrict 比賽作品(成果)

--- app/src/main/java/com/example/a55th_module1_subdistrict/MainActivity.kt
+++ app/src/main/java/com/example/a55th_module1_subdistrict/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a55th_module1_subdistrict
 import android.content.Intent
 import android.os.Bundle
 import android.text.Layout
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -10,6 +11,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var isplaying = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,6 +26,16 @@ class MainActivity : AppCompatActivity() {
         musicListBtn.setOnClickListener {
             val intent = Intent(this,MusicPlayerActivity::class.java)
             startActivity(intent)
+        }
+        val play_Btn = findViewById<ImageButton>(R.id.play_Btn)
+        play_Btn.setOnClickListener {
+            if (isplaying) {
+                play_Btn.setImageResource(R.drawable.outline_play_arrow_24)
+                isplaying = !isplaying
+            }else{
+                play_Btn.setImageResource(R.drawable.sharp_pause_24)
+                isplaying = !isplaying
+            }
         }
     }
 }
